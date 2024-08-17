bb732816c2247dd19150af13feade212e9c6d554
basia333 <bziarnowska@gmail.com>
2024-08-17 18:20:36 +0100

    Update NotificationBar.js

--- src/NotificationBar.js
+++ src/NotificationBar.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './NotificationBar.css'; // Import CSS for notification bar styling
+import './NotificationBar.css'; 
 
 function NotificationBar() {
   return (
     <div className="notification-bar">
       <h2>Notifications</h2>
       <p>No new notifications.</p>
-      {/* Add more notification content or interactive elements here */}
+
     </div>
   );
 }
